cc15e1e95cec60e4ae920a86556a434c303763b5
Noah Huppert <noahhuppert@gmail.com>
2015-12-07 22:32:08 -0500

    Added glfw to build

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-#Glib
+# Glib
 An OpenGl library.
+
+# Building
+In order to build glib you must install some dev dependencies with the following command
+
+```
+sudo apt-get install xorg-dev libgl1-mesa-dev
+```
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <GLFW/glfw3.h>
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+}
